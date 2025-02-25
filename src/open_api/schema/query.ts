63bbd364dda94e4301f9439fed4303023ce26e99
taku10101 <taku1011ht@gmail.com>
2025-02-26 00:04:17 +0900

    Remove unused user and post schemas; update getListQuery to use limit and offset

--- src/open_api/schema/query.ts
+++ src/open_api/schema/query.ts
@@ -1,8 +1,16 @@
 import { z } from "@hono/zod-openapi";
 
-const getListQuery = z.object({
-  page_id: z.string().optional(),
-  page_size: z.string().optional(),
-});
+const getListQuery = z
+  .object({
+    limit: z.number().int().optional(),
+    offset: z.number().int().optional(),
+  })
+  .openapi({
+    example: {
+      limit: 10,
+      offset: 0,
+    },
+  })
+  .describe("GetListQuery");
 
 export { getListQuery };
